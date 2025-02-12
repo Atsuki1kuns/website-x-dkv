e744f06853e68a11b9fba7f0e3c523fdfd8a25c1
Atsuki <cagerytcraft2@gmail.com>
2025-02-12 09:58:36 +0800

    percobaan
    ini adalah percobaan pertama saya membuat sebuah website!!

---
+++ x dkv web script.js
@@ -0,0 +1,16 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    const redirectBtn = document.getElementById('redirectBtn');
+  
+    redirectBtn.addEventListener('click', function() {
+      window.location.href = 'https://www.instagram.com/elite.dkv/';  // Ubah URL ke halaman yang diinginkan
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const redirectBtn1 = document.getElementById('redirectBtn1');
+  
+    redirectBtn1.addEventListener('click', function() {
+      window.location.href = 'http://www.youtube.com/@XIIDKVSKAND';  // Ubah URL ke halaman yang diinginkan
+    });
+  });
